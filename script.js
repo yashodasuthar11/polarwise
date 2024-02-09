6b97f6e61419348d5240b6bed81cfd9b1a11d785
yashodasuthar11 <yashodasuthar11@example.com>
2024-02-09 11:32:10 +0530

    remove console

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "450px";
 }
-console.log("pooja");
+
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
